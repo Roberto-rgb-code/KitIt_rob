346ac64b8583cf0306c56d85f9a8e7429185f505
Javier Gutierrez <gugodin@hotmail.com>
2022-09-08 20:25:11 -0500

    Agregar el boton dentro del mapa

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Info Ruta ',
+      title: 'z',
       debugShowCheckedModeBanner: false,
       initialRoute: 'map',
       routes: 
